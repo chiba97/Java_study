da0edebc2ec16d03f50ff35e27ce47b2e8c1dc28
chiba97 <ktfkyrya@gmail.com>
2022-02-01 07:03:01 +0000

    [Add]java学習

---
+++ java01/Kouhan.java
@@ -0,0 +1,14 @@
+package comment;
+
+public class Kouhan {
+  public static void callDeae() {
+    System.out.println("えぇい、こしゃくな。曲者だ！であえい！");
+  }
+
+  public static void showMondokoro() throws Exception {
+    System.out.println("飛車さん、角さん。もう良いでしょう。");
+    System.out.println("この紋所が目に入らぬか！");
+    Thread.sleep(3000);
+    Zenhan.doTogame();
+  }
+}
